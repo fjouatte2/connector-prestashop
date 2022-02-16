6725931591465b2340488c561a14a061c33d989d
fjouatte <fjouatte@anybox.fr>
2022-02-16 09:35:57 +0100

    pdb

--- connector_prestashop/components/backend_adapter.py
+++ connector_prestashop/components/backend_adapter.py
@@ -183,6 +183,8 @@ class GenericAdapter(AbstractComponent):
 
         :rtype: list
         """
+        import pdb
+        pdb.set_trace()
         _logger.debug(
             'method search, model %s, filters %s',
             self._prestashop_model, filters)
