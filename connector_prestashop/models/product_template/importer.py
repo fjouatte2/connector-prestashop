ba51609bbb4cfb36ce6a67212219a8be5b47d482
fjouatte <fjouatte@anybox.fr>
2022-03-07 14:34:45 +0100

    pdb for testing

--- connector_prestashop/models/product_template/importer.py
+++ connector_prestashop/models/product_template/importer.py
@@ -612,6 +612,8 @@ class ProductTemplateImporter(Component):
         image_count = 0
         prestashop_record = self._get_prestashop_data()
         associations = prestashop_record.get('associations', {})
+        import pdb
+        pdb.set_trace()
         images = associations.get('images', {}).get(
             self.backend_record.get_version_ps_key('image'), {})
         if not isinstance(images, list):
@@ -684,6 +686,8 @@ class ProductTemplateImporter(Component):
     def _import_categories(self):
         record = self.prestashop_record
         associations = record.get('associations', {})
+        import pdb
+        pdb.set_trace()
         categories = associations.get('categories', {}).get(
             self.backend_record.get_version_ps_key('category'), [])
         if not isinstance(categories, list):
